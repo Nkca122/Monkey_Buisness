c1cf8c935000a5c7f0b47c2fc2332c72f37a1c6f
Nkca122 <nkca122@gmail.com>
2024-10-30 04:44:10 +0530

    Addition of index route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from '../pages/layout'
+import Home from '../pages/home'
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout/>}>
               {/* Routes Consisting of the Website */}
+              <Route index element={<Home/>}/>
           </Route>
         </Routes>
       </Router>
