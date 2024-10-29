3c1e41e8b1bb13e03b43e3d81793ace7f6167f1b
Nkca122 <nkca122@gmail.com>
2024-10-29 22:51:04 +0530

    Layout Init

---
+++ frontend/components/footer.jsx
@@ -0,0 +1,22 @@
+export default function Footer(){
+    return (
+        <>
+            <div className="f-div">
+                <div className="f-socials-div">
+                    <ul>
+                        <p>Linked In</p>
+                        <p>Github</p>
+                        <p>Instagram</p>
+                    </ul>
+                </div>
+                <div className="f-logo-div">
+                    <img src="" alt="Logo" />
+                </div>
+                <div className="f-info-div">
+                    <p>Copyright</p>
+                    <p>TradeMark</p>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ frontend/components/header.jsx
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+export default function Header(){
+    return (
+        <>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+            </ul>
+        </>
+    )
+}
