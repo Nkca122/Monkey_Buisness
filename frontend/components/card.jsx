77eb602266503d0a2665dd7e96a9633e5daf5619
Nkca122 <nkca122@gmail.com>
2024-11-01 02:21:12 +0530

    Card component

---
+++ frontend/components/card.jsx
@@ -0,0 +1,54 @@
+export default function Card(props) {
+  let imgSrc = props.img;
+  let heading = props.heading;
+  let description = props.description;
+  let link = props.link;
+  return (
+    <>
+      {link ? (
+        <a href={link}>
+          <div className="card-div container">
+            {heading ? <h3>{heading}</h3> : null}
+            {imgSrc ? (
+              <div
+                className="card-img"
+                style={{
+                  backgroundImage: `url(${imgSrc})`,
+                }}
+              ></div>
+            ) : null}
+            {description ? (
+              <div className="card-description">
+                <p>{description}</p>
+              </div>
+            ) : null}
+            {link ? (
+            <div className="card-link">
+              <p href={link}>Try It!</p>
+            </div>
+          ) : null}
+          </div>
+        </a>
+      ) : (
+        <a href="#">
+          <div className="card-div container">
+            {heading ? <h3>{heading}</h3> : null}
+            {imgSrc ? (
+              <div
+                className="card-img"
+                style={{
+                  backgroundImage: `url(${imgSrc})`,
+                }}
+              ></div>
+            ) : null}
+            {description ? (
+              <div className="card-description">
+                <p>{description}</p>
+              </div>
+            ) : null}
+          </div>
+        </a>
+      )}
+    </>
+  );
+}
